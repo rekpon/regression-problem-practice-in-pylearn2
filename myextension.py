42a74b97044481f4d75950de94f90bb7599d6694
rekpon <rekponke@gmail.com>
2015-10-11 02:20:30 +0900

    pylearn2で行う回帰問題の簡単なサンプル
    [add] train, myextension を追加
    
    train.pyは学習に、myextension.pyは学習経過の表示に必要

---
+++ myextension.py
@@ -0,0 +1,70 @@
+#coding: utf-8
+""" pylearn2 用 extensions
+[PlotPredictionOnMonitor]
+- epoch毎にテスト用データに対するモデルの予測値をプロットし、遷移をアニメーション表示する
+- アニメーションの開始には<this_extension>.plot()
+"""
+
+import random
+import numpy as np
+import theano
+import pylearn2
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import matplotlib.animation as animation
+from math import pi, cos, sin
+from pylearn2.train_extensions import TrainExtension
+
+# extend range function
+def drange(begin, end, step):
+    n = begin
+    while n+step < end:
+        yield n
+        n += step
+
+class PlotPredictionOnMonitor(TrainExtension):
+    def setup(self, model, dataset, algorithm):
+        self.model = model
+        self.algorithm = algorithm
+
+        # figure plot setup
+        self.fig = plt.figure()
+        ax = self.fig.add_subplot(111)
+        ax.grid(True,linestyle='-', color='0.75')
+        ax.set_xlim([0, 2*pi])
+
+        ## make test dataset
+        self.test_data = []
+        for i in drange(0, 6.29, 0.01):
+            self.test_data.append([i])
+        self.test_data = np.array(self.test_data)
+
+        ## save initial output
+        self.frame_list = []
+        output_data = self.model.fprop(theano.shared(self.test_data, name='inputs')).eval()
+        pylab.plot(self.test_data, output_data, '-', color='r')
+
+    def on_monitor(self, model, dataset, algorithm):
+        # plot model output on every epoch
+        output_data = self.model.fprop(theano.shared(self.test_data, name='inputs')).eval()
+        one_frame = pylab.plot(self.test_data, output_data, '-', color='k')
+        self.frame_list.append(one_frame)
+        del output_data
+
+    # plot sincurve and start animation
+    def plot(self, out_filename=None):
+        x = np.arange(0, 2*pi+0.1, 0.1)
+        y = np.sin(x)
+        pylab.plot(x, y, color='b', label="sin(x)")
+
+        # animation
+        print("Pless Enter to start animation")
+        input()
+        anim = animation.ArtistAnimation(self.fig,self.frame_list,
+                                         interval=100,
+                                         repeat_delay=10000)
+        if out_filename is not None:
+            print("saving animation to", out_filename+".mp4", "...")
+            anim.save(out_filename+".mp4", writer="ffmpeg", fps=24)
+            print("done.")
+        plt.show()
---
+++ train.py
@@ -0,0 +1,109 @@
+#coding: utf-8
+""" pylearn2 でsin関数を近似するサンプルプログラム。
+
+[実行方法]
+$ python train.py
+
+[オプション]
+-p : epoch毎にモデルの予測値を保存、学習終了後にアニメーションで遷移を表示。
+
+-f, --file <finename> : -pオプションのアニメーションをmp4ファイルで保存(要ffmpeg)
+
+[出力]
+学習結果は ./funcmodel.pkl に保存。
+
+[note]
+1. MAX_EPOCH は学習データを繰り返し学習する回数。
+   SIZE_DATA は作成する学習データの個数。
+
+2. 近似する関数を変えるにはUnknownFunk()のX,yを変更。
+   Xが入力値、yが出力値の2-dimention nparray。
+
+3. 各隠れ層のノード数や層数によりtrainerのlearning_rateの調節が必要。
+"""
+
+MAX_EPOCHS = 100
+SIZE_DATA = 200
+
+import random
+import numpy as np
+import pylearn2
+import time
+from math import pi, cos, sin
+from pylearn2.termination_criteria import EpochCounter
+from pylearn2.training_algorithms import sgd
+from pylearn2.datasets import DenseDesignMatrix
+from pylearn2.models import mlp
+from pylearn2.train import Train
+from optparse import OptionParser
+from myextension import PlotPredictionOnMonitor
+
+# 学習させる未知の関数の入出力ペアを生成
+class sinDataset(DenseDesignMatrix):
+    def __init__(self, n):
+        X = []
+        y = []
+        for i in range(0, n):
+            x = random.uniform(-0.5, 2*pi+0.5)
+            X.append([x])
+            y.append([sin(x)])
+
+        X = np.array(X)
+        y = np.array(y)
+        super(sinDataset, self).__init__(X=X, y=y)
+
+def main():
+    start_time = time.clock()
+
+    # optin parser
+    parser = OptionParser()
+    parser.add_option("-p", dest="plot_prediction",
+                      action="store_true", default=False,
+                      help="plot model prediction transitions")
+    parser.add_option("-f", "--file", dest="out_filename", default=None,
+                      help="write animation to FILE (require -a option)", metavar="FILE")
+    (options, args) = parser.parse_args()
+
+    # make Detaset
+    ds = sinDataset(SIZE_DATA)
+
+    # definition layers
+    hidden_layer1 = mlp.Tanh(layer_name='hidden1', dim=20, irange=0.5, init_bias=1.0)
+    hidden_layer2 = mlp.Tanh(layer_name='hidden2', dim=4, irange=0.5, init_bias=1.0)
+    output_layer = mlp.Linear(layer_name='out', dim=1, irange=0.5, init_bias=1)
+
+    # set layers
+    layers = [hidden_layer1, hidden_layer2, output_layer]
+    model = mlp.MLP(layers, nvis=1)
+
+    # set training rule and extensions
+    algorithm = sgd.SGD(
+        learning_rate = 0.01,
+        batch_size = 1,
+        monitoring_batch_size = 1,
+        monitoring_batches =  1,
+        monitoring_dataset = ds,
+        termination_criterion = EpochCounter(MAX_EPOCHS)
+    )
+    extensions = [sgd.MonitorBasedLRAdjuster()]
+    if options.plot_prediction:
+        plotEx = PlotPredictionOnMonitor()
+        extensions.append(plotEx)
+
+    trainer = Train(model = model,
+                    algorithm = algorithm,
+                    dataset = ds,
+                    extensions = extensions,
+                    save_path='./funcmodel.pkl',
+                    save_freq=500)
+
+    # training loop
+    trainer.main_loop()
+
+    end_time = time.clock()
+    print("tortal_seconds_this_learning : %f s(%f min)" % (end_time - start_time, (end_time - start_time)/60))
+    if options.plot_prediction:
+        plotEx.plot(out_filename=options.out_filename)
+
+if __name__ == '__main__':
+    main()
